de570b6309eb6f9f39d4fe5b63587d8c7e43d4bc
Simrandeep kour <136782054+kour1210@users.noreply.github.com>
2025-02-04 10:59:51 +0000

    Variables, datatypes and conversions done

---
+++ 01_basics/conversions.js
@@ -0,0 +1,7 @@
+let ob = {name:"john", dob:"12/10/1999" }
+
+let num = true
+let str = String(num)
+console.log(typeof num)
+console.log(str, typeof str)
+
---
+++ 01_basics/variablesDatatypes.js
@@ -0,0 +1,17 @@
+const accountId = 121
+let accountName = "Sam"
+var accountPassword = "Sam@121"
+accountCity = "Hyderabad"
+console.table([accountId, accountName, accountPassword, accountCity])
+
+ accountName = "John"
+ accountPassword = "John121"
+ accountCity = "Noida"
+
+ console.table([accountId, accountName, accountPassword, accountCity])
+
+ let note = "Use only const and let in JS, because let supports scopes in JS"
+
+ let note2 = "There are 6 datatypes : Number, String, Boolean, Symbol, Object and Undefined in which NULL is standard value whose type is Object"
+
+ console.table([note, note2])
